91fa451c02b63450a8a70b68874fa98a016dc189
somercelik <dovahkiinborn@outlook.com>
2020-07-20 18:45:46 +0300

    Ilk gönderim

---
+++ app/src/main/java/com/somercelik/countdown/MainActivity.java
@@ -0,0 +1,33 @@
+package com.somercelik.countdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long l) {
+                textView.setText("Left: " + l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG);
+                textView.setText("No time left.");
+            }
+        }.start();
+
+    }
+}
